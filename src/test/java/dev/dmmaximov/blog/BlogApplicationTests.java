936d4f411703da8bacf263f4111218d1feb4bcb7
dezmontx <dezmontorigin@gmail.com>
2022-05-10 21:11:41 +0700

    Initial commit

---
+++ src/test/java/dev/dmmaximov/blog/BlogApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.dmmaximov.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
